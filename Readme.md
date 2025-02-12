d842e6b1a72df74021e25f8b6a404dfe7de9fa5a
rodfgmoreira <rodfgmoreira@gmail.com>
2025-02-12 19:47:05 -0300

    Beta version 0.1.0

--- Readme.md
+++ Readme.md
@@ -1,1 +1,12 @@
-test
+## This is a small Infinite Run game project for my studies
+
+## Changelogs
+
+### v0.1.0 - (12/02/2025)
+- First playable version 
+
+### v0.1.1 - (13/02/2025)
+- Score Counting: Added a loop that increases the score every 100ms while the game is running.
+- Visual Update: The score now correctly appears on the screen.
+- Saved High Score: If the player sets a new record, it is saved in localStorage and loaded in the next session.
+- Score Stops on Game Over: Prevents the score from increasing after the game ends.
--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 const character = document.querySelector('.character');
+const box = document.querySelector('.boxA');
+const gameoverTitle = document.querySelector('.gameover-title')
+const restartBtn = document.querySelector('.restartBtn')
+const clouds = document.querySelector('.clouds')
+const scoreElement = document.querySelector('.score-points')
+const highScoreElement = document.querySelector('.score-high')
+
+let currentScore = 0;
+let bestScore = localStorage.getItem('highScore') || 0;
+let gameRunning = true;
+
+// Show saved Highscore when start
+highScoreElement.innerText = bestScore;
+
+// Score function
+const scoreMeter = () => {
+    if (gameRunning) {
+        currentScore++;
+        scoreElement.innerText = currentScore;
+    }
+}
+
+const scoreLoop = setInterval(scoreMeter, 100);
 
 const jump = () => {
     character.classList.add('jump');
@@ -7,5 +30,45 @@ const jump = () => {
         character.classList.remove('jump');
     }, 525);
 }
+
+const restart = () => {
+    window.location.reload()
+}
+
+const loop = setInterval(() => {
+    const boxPosition = box.offsetLeft;
+    const cloudsPosition = clouds.offsetLeft;
+    const characterPosition = +window.getComputedStyle(character).bottom.replace('px', '');
+
+    if (boxPosition <= 120 && boxPosition > 0 && characterPosition < 110) {
+        gameRunning = false;
+        if (currentScore > bestScore) {
+            bestScore = currentScore;
+            localStorage.setItem('highScore', bestScore);
+            highScoreElement.innerText = bestScore;
+        }
+
+        // Animations
+        box.style.animation = 'none';
+        box.style.left = `${boxPosition}px`
+
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`
+
+        character.style.animation = 'none';
+        character.style.bottom = `${characterPosition}px`;
+
+        character.src = './images/game-over.png';
+        character.style.width = '75px';
+        character.style.marginLeft = '50px';
+
+        gameoverTitle.style.opacity = '100';
+        restartBtn.style.opacity = '100';
+
+        clearInterval(loop);
+        clearInterval(scoreLoop);
+    }
+
+}, 10);
 
 document.addEventListener('keydown', jump);
